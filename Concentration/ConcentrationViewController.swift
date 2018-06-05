4478cbf8d9a2155f6ebc7154eb3c0ab09d3e4f64
davidzengming <davidzengming@gmail.com>
2018-06-05 11:36:07 -0400

    added theme changer and additional view controllers

---
+++ Concentration/ConcentrationViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by David Zeng on 2/17/18.
+//  Copyright © 2018 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class ConcentrationViewController: UIViewController {
+    private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
+    
+    var numberOfPairsOfCards: Int {
+        get {
+            return (cardButtons.count + 1) / 2
+        }
+    }
+    
+    private(set) var flipCount = 0 { didSet { flipCountLabel.text = "Flips: \(flipCount)" } }
+    
+    @IBOutlet private weak var flipCountLabel: UILabel!
+    
+    @IBOutlet private var cardButtons: [UIButton]!
+    
+    @IBAction private func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
+        } else {
+            print("chosen card was not in cardButtons")
+        }
+    }
+    
+    private func updateViewFromModel() {
+        if cardButtons != nil {
+            for index in cardButtons.indices {
+                let button = cardButtons[index]
+                let card = game.cards[index]
+                if card.isFaceUp {
+                    button.setTitle(emoji(for: card), for: UIControlState.normal)
+                    button.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+                } else {
+                    button.setTitle("?", for: UIControlState.normal)
+                    button.backgroundColor = card.isMatched ? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0) : #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
+                }
+            }
+        }
+    }
+    
+    var theme: String? {
+        didSet {
+            emojiChoices = theme ?? ""
+            emoji = [:]
+            updateViewFromModel()
+        }
+    }
+    
+    private var emojiChoices = "👿😿👻😾🤖😎😒"
+    
+    private var emoji = [Card:String]()
+    
+    private func emoji(for card: Card) -> String {
+        if emoji[card] == nil, emojiChoices.count > 0 {
+            let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4random)
+            emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
+        }
+        return emoji[card] ?? "?"
+    }
+}
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if (self < 0) {
+            return -Int(arc4random_uniform(UInt32(self)))
+        } else {
+            return 0
+        }
+    }
+}
+
---
+++ Concentration/ConcontrationThemeChooserViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ConcontrationThemeChooserViewController.swift
+//  Concentration
+//
+//  Created by David Zeng on 6/4/18.
+//  Copyright © 2018 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class ConcontrationThemeChooserViewController: UIViewController, UISplitViewControllerDelegate {
+    
+    let themes = [
+        "Sports":"⚽️🏀🏈⚾️🎾🏐🏉",
+        "Animals":"🐔🐷🦊🐵🐻🙊🐶",
+        "Faces":"👿😿👻😾🤖😎😒"
+    ]
+    
+    override func awakeFromNib() {
+        splitViewController?.delegate = self
+    }
+    
+    func splitViewController(_ splitViewController: UISplitViewController,
+                             collapseSecondary secondaryViewController: UIViewController,
+                             onto primaryViewController: UIViewController) -> Bool {
+        if let cvc = secondaryViewController as? ConcentrationViewController {
+            if cvc.theme == nil {
+                return true
+            }
+        }
+        return false
+    }
+    
+    @IBAction func changeTheme(_ sender: Any) {
+        if let cvc = splitViewDetailConcentrationViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                cvc.theme = theme
+            }
+        } else if let cvc = lastSeguedToConcentrationViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                cvc.theme = theme
+            }
+            navigationController?.pushViewController(cvc, animated: true)
+        } else {
+            performSegue(withIdentifier: "Choose Theme", sender: sender)
+        }
+    }
+    
+    private var splitViewDetailConcentrationViewController: ConcentrationViewController? {
+        return splitViewController?.viewControllers.last as? ConcentrationViewController
+    }
+    
+    private var lastSeguedToConcentrationViewController: ConcentrationViewController?
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "Choose Theme" {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                if let cvc = segue.destination as? ConcentrationViewController {
+                    cvc.theme = theme
+                    lastSeguedToConcentrationViewController = cvc
+                }
+            }
+        }
+    }
+}
